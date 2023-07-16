77d9d1b3f4802dced8fc37858717dcc5fe054752
mudit gupta <muditgupta68@gmail.com>
2023-07-16 23:30:40 +0530

    req data

--- myApp.py
+++ myApp.py
@@ -1,12 +1,16 @@
-from flask import Flask
+from flask import Flask, request
 import joblib
 app = Flask(__name__)
 
-@app.route('/',methods=['GET'])
-def hello_world():
+@app.route('/churn',methods=['GET','POST'])
+def basic():
+    if request.method == 'POST':
+        res = request.form
+        print(res)
     model = joblib.load('rf_churn')
     pred = model.predict([[619,42,2,0.0,0,0,0,101348.88,0,0,0]])
     return f"Prediction is : {pred[0]}";
+
 
 if __name__ == '__main__':
 	app.run()
