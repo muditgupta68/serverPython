b4dad8b8a31edb772bd2d506d5fbcf5efea967c0
mudit gupta <muditgupta68@gmail.com>
2023-07-16 23:06:06 +0530

    req data

--- myApp.py
+++ myApp.py
@@ -4,8 +4,9 @@ app = Flask(__name__)
 
 @app.route('/',methods=['GET'])
 def hello_world():
-    model = joblib.load('rf_heartForecastModel')
-    return 'Model Loaded Success!'
+    model = joblib.load('rf_churn')
+    pred = model.predict([[619,42,2,0.0,0,0,0,101348.88,0,0,0]])
+    return f"Prediction is : {pred[0]}";
 
 if __name__ == '__main__':
 	app.run()
